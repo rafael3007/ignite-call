cfa92a37d3c35ddce84e0c10b9d58976c2b72187
Rafael Brito <rafael.brito.1422@gmail.com>
2025-04-17 10:53:58 -0300

    init

---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { globalCss } from "@ignite-ui/react"
+
+export const globalStyles = globalCss({
+  '*': {
+    boxSizing: 'border-box',
+    margin: 0,
+    padding: 0,
+    // textDecoration: 'none',
+  },
+
+  body: {
+    backgroundColor: '$gray900',
+    color: '$gray100',
+    '-webkit-font-smoothing': 'antialiased',
+    fontFamily: 'Roboto, sans-serif',
+  },
+
+  'body, input, textarea, button': {
+    fontWeight: 400,
+    fontSize: '1rem',
+  },
+
+  
+})
