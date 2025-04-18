a535eefeed9c3b4ca7567d5f826022b726e8a304
rafael3007 <rafael.brito.1422@gmail.com>
2025-04-18 20:57:05 -0300

    feat: home validations

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,34 +1,48 @@
 import { Button, TextInput } from '@ignite-ui/react'
-import { Form } from './styles'
+import { Form, FormAnotation } from './styles'
 import { ArrowRight } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { claimUsernameFormSchema } from '@/src/pages/home/validators/claim-username-form-schema'
 
 type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export default function ClaimUsernameForm() {
-  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  })
 
   async function handleClaimUsername(data: ClaimUsernameFormData) {
     console.log(data)
   }
 
   return (
-    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
-      <TextInput
-        onPointerEnterCapture={undefined}
-        onPointerLeaveCapture={undefined}
-        crossOrigin={undefined}
-        {...register('username')}
-        prefix="ignite.com/"
-        placeholder="seu-usuario"
-        size="sm"
-      />
-      <Button type="submit" size="sm">
-        Reservar usuário
-        <ArrowRight weight="bold" />
-      </Button>
-    </Form>
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+          crossOrigin={undefined}
+          {...register('username')}
+          prefix="ignite.com/"
+          placeholder="seu-usuario"
+          size="sm"
+        />
+        <Button type="submit" size="sm">
+          Reservar usuário
+          <ArrowRight weight="bold" />
+        </Button>
+      </Form>
+      <FormAnotation>
+        {errors.username
+          ? errors.username.message
+          : 'Digite o nome do usuário desejado'}
+      </FormAnotation>
+    </>
   )
 }
