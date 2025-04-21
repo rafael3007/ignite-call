e3d70e957fbc9158d11eb391268762da8e83ab01
rafael3007 <rafael.brito.1422@gmail.com>
2025-04-21 18:09:15 -0300

    feat: route available times scheduling

---
+++ src/pages/api/users/[username]/availability.api.ts
@@ -0,0 +1,88 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '../../../../lib/prisma'
+import dayjs from 'dayjs'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'GET') {
+    return res.status(405).end()
+  }
+  const username = String(req.query.username)
+  const { date } = req.query
+
+  if (!date) {
+    return res.status(400).json({ error: 'Date not provided.' })
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return res.status(404).json({ error: 'User does not exist.' })
+  }
+
+  const referenceDate = dayjs(String(date))
+
+  const isPastDate = referenceDate.endOf('day').isBefore(new Date())
+
+  if (isPastDate) {
+    return res.json({ availability: [] })
+  }
+
+  const userAvailability = await prisma.userTimeInterval.findFirst({
+    where: {
+      user_id: user.id,
+      week_day: referenceDate.get('day'),
+    },
+  })
+
+  if (!userAvailability) {
+    return res.json({ availability: [] })
+  }
+
+  const {
+    end_time_in_minutes: endTimeInMinutes,
+    start_time_in_minutes: startTimeInMinutes,
+  } = userAvailability
+
+  const startHour = startTimeInMinutes / 60
+  const endHour = endTimeInMinutes / 60
+
+  const possibleTimes = Array.from({ length: endHour - startHour }).map(
+    (_, i) => {
+      return startHour + i
+    },
+  )
+
+  const blockedTimes = await prisma.scheduling.findMany({
+    select: {
+      date: true,
+    },
+    where: {
+      user_id: user.id,
+      date: {
+        gte: referenceDate.set('hour', startHour).toDate(),
+        lte: referenceDate.set('hour', endHour).toDate(),
+      },
+    },
+  })
+
+  const availableTimes = possibleTimes.filter((time) => {
+    return !blockedTimes.some((blockedTime) => {
+      return (
+        blockedTime.date.getHours() === time
+        // || blockedTime.date.getHours() + 1 === time
+      )
+    })
+  })
+
+  return res.json({
+    possibleTimes,
+    availableTimes,
+  })
+}
