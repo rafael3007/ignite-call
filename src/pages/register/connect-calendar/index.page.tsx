a0911aeabfc83b212d328ed508c869f8789df159
rafael3007 <rafael.brito.1422@gmail.com>
2025-04-22 19:29:07 -0300

    fix: routes

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -18,7 +18,7 @@ export default function ConnectCalendar() {
   }
 
   async function handleNavigateToNextStep() {
-    await router.push(`/register/update-profile`)
+    await router.push(`/register/time-intervals`)
   }
 
   return (
